de8d5f50253929f9dbe91f48fe683faa44ec1f80
sofia-lpz <a01028251@tec.mx>
2025-07-09 14:41:41 -0600

    removed node server

--- backend/api/router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import * as controller from './controller.js'
-
-const router = express.Router();
-
-router.post('/login', controller.login);
-router.post('/register', controller.register);
-
-export { router }
