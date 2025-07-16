b74e9a4fc8a98da4fca3ebbcb53589fe721697a2
Juan de Dios <juandediosgastelum@hotmail.com>
2025-07-16 10:55:03 -0500

    feat: sección misión y visión en /acerca-de-nosotros + dependencias

---
+++ nextapp/src/app/acerca-de-nosotros/page.jsx
@@ -0,0 +1,9 @@
+import AboutText from "@/components/about/AboutText";
+
+export default function AboutPage() {
+  return (
+    <main className="px-6 py-12">
+      <AboutText />
+    </main>
+  );
+}
