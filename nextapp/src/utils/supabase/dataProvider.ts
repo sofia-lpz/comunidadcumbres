1adeae1935f69a1f72298c1675ac21b73eded459
sofia-lpz <a01028251@tec.mx>
2025-08-02 09:25:32 -0600

    installed aws amplify

--- nextapp/src/utils/supabase/dataProvider.ts
+++ nextapp/src/utils/supabase/dataProvider.ts
@@ -4,4 +4,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = secret('SUPABASE_URL');
 const supabaseKey = secret('SUPABASE_KEY');
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
