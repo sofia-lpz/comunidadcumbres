30f40c2a02b3102f45f8c4522796ba8b31426e3c
Hector Lugo <hlugogabino@gmail.com>
2025-12-25 18:22:29 -0600

    fix: mixpanel and next errors

--- nextapp/src/utils/supabase/dataProvider.ts
+++ nextapp/src/utils/supabase/dataProvider.ts
@@ -1,19 +1,7 @@
-import { secret } from '@aws-amplify/backend';
 import { createClient } from '@supabase/supabase-js';
-import { defineFunction } from '@aws-amplify/backend';
-
-const supabaseUrl = secret('SUPABASE_URL');
-const supabaseKey = secret('SUPABASE_KEY');
-
-export const myFunction = defineFunction({
-  environment: {
-    SUPABASE_URL: supabaseUrl,
-    SUPABASE_KEY: supabaseKey,
-  },
-});
 
 export async function getSupabaseClient() {
-  const url = process.env.SUPABASE_URL!;
-  const key = process.env.SUPABASE_KEY!;
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   return createClient(url, key);
 }
